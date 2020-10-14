b34650aa1c61c328185b417c857e0667ca17915c
Vivek Maskara <maskaravivek@gmail.com>
2020-10-14 02:29:20 -0700

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+
+var mapSvg;
+
+var lineSvg;
+var lineWidth;
+var lineHeight;
+var lineInnerHeight;
+var lineInnerWidth;
+var lineMargin = { top: 20, right: 60, bottom: 60, left: 100 };
+
+var mapData;
+var timeData;
+
+// This runs when the page is loaded
+document.addEventListener('DOMContentLoaded', function() {
+  mapSvg = d3.select('#map');
+  lineSvg = d3.select('#linechart');
+  
+  // Load both files before doing anything else
+  Promise.all([d3.json('data/africa.geojson'),
+               d3.csv('data/africa_gdp_per_capita.csv')])
+          .then(function(values){
+    
+    mapData = values[0];
+    timeData = values[1];
+   
+    drawMap();
+  })
+
+});
+
+// Draw the map in the #map svg
+function drawMap() {
+  
+}
+
+
+// Draw the line chart in the #linechart svg for
+// the country argument (e.g., `Algeria').
+function drawLineChart(country) {
+
+  if(!country)
+    return;
+  
+}
