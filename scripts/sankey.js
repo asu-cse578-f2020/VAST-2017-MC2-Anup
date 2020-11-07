302e2ba701840100c2dc06bb4c08b9e20ad0d109
raul9595 <rahulsvarm@gmail.com>
2020-11-06 22:41:05 -0700

    Intial commit for Alluvial chart

---
+++ scripts/sankey.js
@@ -0,0 +1,72 @@
+// set the dimensions and margins of the graph
+const margin = {top: 20, right: 40, bottom: 20, left: 40};
+const outerWidth = 600;
+const outerHeight = 520;
+const width = outerWidth - margin.left - margin.right;
+const height = outerHeight - margin.top - margin.bottom;
+
+
+d3.json("data/sankey.json").then(function (data) {
+
+    var svg = d3.select("#sankey").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var sankey = d3.sankey()
+        .nodeWidth(20)
+        .nodePadding(10)
+        .size([width, height]);
+
+    sankey
+        .nodes(data.nodes)
+        .links(data.links)
+        .layout(1);
+
+    var link = svg.append("g")
+        .selectAll(".link")
+        .data(data.links)
+        .enter()
+        .append("path")
+        .attr("class", "link")
+        .attr("d", sankey.link())
+        .attr('opacity', 0.2)
+        .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+        .sort(function(a, b) { return b.dy - a.dy; });
+
+    var node = svg.append("g")
+        .selectAll(".node")
+        .data(data.nodes)
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .call(d3.drag()
+            .subject(function(d) { return d; })
+            .on("start", function() { this.parentNode.appendChild(this); }))
+
+    node
+        .append("rect")
+        .attr("height", function(d) { return d.dy; })
+        .attr("width", sankey.nodeWidth())
+        .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
+        .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+        .append("title")
+        .text(function(d) { return d.name + "\n" + "There is " + d.value + " stuff in this node"; });
+
+    node
+        .append("text")
+        .attr("x", -6)
+        .attr("y", function(d) { return d.dy / 2; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", "end")
+        .attr("transform", null)
+        .text(function(d) { return d.name; })
+        .filter(function(d) { return d.x < width / 2; })
+        .attr("x", 6 + sankey.nodeWidth())
+        .attr("text-anchor", "start");
+
+});
